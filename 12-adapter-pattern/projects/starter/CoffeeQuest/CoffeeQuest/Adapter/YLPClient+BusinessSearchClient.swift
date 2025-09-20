05e5803a058ac7f87dd082ab7e754ab402ee0b3f
Tan Ngo Dang <ngodangtan1994@gmail.com>
2025-09-20 17:09:22 +0700

    update

--- 12-adapter-pattern/projects/starter/CoffeeQuest/CoffeeQuest/Adapter/YLPClient+BusinessSearchClient.swift
+++ 12-adapter-pattern/projects/starter/CoffeeQuest/CoffeeQuest/Adapter/YLPClient+BusinessSearchClient.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2019 Razeware LLC
+/// Copyright (c) 2025 Razeware LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -26,36 +26,55 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import UIKit
 import MapKit
 import YelpAPI
-
-public class AnnotationFactory {
+// 1
+extension YLPClient: BusinessSearchClient {
+  public func search(with coordinate: CLLocationCoordinate2D,
+                     term: String,
+                     limit: UInt,
+                     offset: UInt,
+                     success: @escaping (([Business]) -> Void),
+                     failure: @escaping ((Error?) -> Void)) {
+    // 2
+    let yelpCoordinate = YLPCoordinate(
+      latitude: coordinate.latitude,
+      longitude: coordinate.longitude)
   
-  public func createBusinessMapViewModel(for business: Business) -> BusinessMapViewModel {
-    let coordinate = business.location
-  
-    let name = business.name
-    let rating = business.rating
-    let image: UIImage
-    switch rating {
-    case 0.0..<3.0:
-      image = UIImage(named: "terrible")!
-    case 3.0..<3.5:
-      image = UIImage(named: "bad")!
-    case 3.5..<4.0:
-      image = UIImage(named: "meh")!
-    case 4.0..<4.75:
-      image = UIImage(named: "good")!
-    case 4.75...5.0:
-      image = UIImage(named: "great")!
-    default:
-      image = UIImage(named: "bad")!
+    search(
+      with: yelpCoordinate,
+      term: term,
+      limit: limit,
+      offset: offset,
+      sort: .bestMatched,
+      completionHandler: { (searchResult, error) in
+        // 3
+        guard let searchResult = searchResult,
+              error == nil else {
+          failure(error)
+          return
+        }
+        // 4
+        let businesses =
+        searchResult.businesses.adaptToBusinesses()
+        success(businesses)
+      })
+  }
+}
+// 5
+extension Array where Element: YLPBusiness {
+  func adaptToBusinesses() -> [Business] {
+    return compactMap { yelpBusiness in
+      guard let yelpCoordinate =
+              yelpBusiness.location.coordinate else {
+        return nil
+      }
+      let coordinate = CLLocationCoordinate2D(
+        latitude: yelpCoordinate.latitude,
+        longitude: yelpCoordinate.longitude)
+      return Business(name: yelpBusiness.name,
+                      rating: yelpBusiness.rating,
+                      location: coordinate)
     }
-
-    return BusinessMapViewModel(coordinate: coordinate,
-                                image: image,
-                                name: name,
-                                rating: rating)
   }
 }
